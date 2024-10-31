ef8051a22eb1c0dbb0d2ba7d0ce120e832bf7eb9
htadmg <agatadominguestec@hotmail.com>
2024-10-31 09:23:09 -0300

    criando vies de get e post para adicionar generos e para listar generos

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from django.http import JsonResponse
-
+from genres.views import genre_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
+    path('genres/', genre_view, name='genere')
 ]
 
